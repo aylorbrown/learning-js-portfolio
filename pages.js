572c7b463d74f324aa0e2a9660263c44b384b6d0
Aylor Brown <aylorbrown@gmail.com>
2020-01-06 10:55:46 -0500

    cleans up code with functions

--- pages.js
+++ pages.js
@@ -3,10 +3,22 @@
 // tag.innerHTML = 'this is a test';
 
 // tag.style.color = 'red';
+let pageNumber = 0;
+
 
 const nextTag = document.querySelector('footer img.next');
 const outputTag = document.querySelector('h2');
 
+const next = function () {
+    pageNumber = pageNumber + 1; 
+}
+
+const updateSection = function () {
+    outputTag.innerHTML = pageNumber;
+}
+
 nextTag.addEventListener('click', function () {
-    outputTag.innerHTML = 'it worked!';
+    
+    next()
+    updateSection();
 })
