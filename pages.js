c6ab84430f28f81b77ee51916e133581eefdd947
Aylor Brown <aylorbrown@gmail.com>
2020-01-06 09:48:01 -0500

    adds event listener for next arrow

--- pages.js
+++ pages.js
@@ -1,5 +1,12 @@
-const tag = document.querySelector('h2');
+// const tag = document.querySelector('h2');
 
-tag.innerHTML = 'this is a test';
+// tag.innerHTML = 'this is a test';
 
-tag.style.color = 'red';
+// tag.style.color = 'red';
+
+const nextTag = document.querySelector('footer img.next');
+const outputTag = document.querySelector('h2');
+
+nextTag.addEventListener('click', function () {
+    outputTag.innerHTML = 'it worked!';
+})
