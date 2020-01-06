db009cc78eadcf109e688d303a6d944110b99992
Aylor Brown <aylorbrown@gmail.com>
2020-01-06 11:06:41 -0500

    adds previous functionality

--- pages.js
+++ pages.js
@@ -1,24 +1,38 @@
-// const tag = document.querySelector('h2');
-
-// tag.innerHTML = 'this is a test';
-
-// tag.style.color = 'red';
 let pageNumber = 0;
 
-
 const nextTag = document.querySelector('footer img.next');
+const previousTag = document.querySelector('footer img.prev');
 const outputTag = document.querySelector('h2');
+
 
 const next = function () {
     pageNumber = pageNumber + 1; 
+
+    if (pageNumber > 10) {
+        pageNumber = 1;
+    }
+    updateSection();
 }
+
+const previous = function () {
+    pageNumber = pageNumber - 1; 
+
+    if (pageNumber < 1) {
+        pageNumber = 10;
+    }
+    updateSection();
+}
+
 
 const updateSection = function () {
     outputTag.innerHTML = pageNumber;
 }
 
 nextTag.addEventListener('click', function () {
-    
     next()
-    updateSection();
 })
+
+previousTag.addEventListener('click', function () {
+    previous();
+})
+
